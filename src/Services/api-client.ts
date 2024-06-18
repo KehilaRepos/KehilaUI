e7f7f6011c3a75a267d0262f9f38d71374e93934
ciciansiha <69649117+ciciansiha@users.noreply.github.com>
2024-06-18 22:05:02 +0300

    fix opener in mobile

--- src/Services/api-client.ts
+++ src/Services/api-client.ts
@@ -1,7 +1,7 @@
 import axios, { CanceledError } from "axios"
 
 export default axios.create({
-    baseURL: "http://kehilaserverbeanstalk-env.eba-sruyaeey.us-east-1.elasticbeanstalk.com"
+    baseURL: "https://kehilaserverbeanstalk-env.eba-sruyaeey.us-east-1.elasticbeanstalk.com"
 });
 
 export { CanceledError };
